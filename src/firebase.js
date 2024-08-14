8085936a0605a572e8a88bb5cd017191cd3c6fd0
LakshyaRinwa <131532132+LakshyaRinwa@users.noreply.github.com>
2024-08-14 12:38:28 +0530

    Making clone of amazon till now home page is designed

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCw6cUSg673oFYjgVymaEKjKfLsyFZgFnM",
+    authDomain: "e-clone15.firebaseapp.com",
+    projectId: "e-clone15",
+    storageBucket: "e-clone15.appspot.com",
+    messagingSenderId: "431710433350",
+    appId: "1:431710433350:web:ddf4a9b5b0a7ca841ada47",
+    measurementId: "G-4PML6NSBWK"
+  };
